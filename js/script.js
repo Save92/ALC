32c29b5b3f6091a0b1cca483331631d572c895e4
Nosista <bjneji@gmail.com>
2013-04-26 19:04:14 +0200

    Galerie choix

--- js/script.js
+++ js/script.js
@@ -1,37 +1,39 @@
 $(document).ready(function (){
 	$(".galerie_choix").hide();
 	$(".categorie").hide();
+
+	$('#featured').orbit();
+	
+	$("#background").css("opacity",0);
+
+
 	$("#galerie").hover(function(){
 		$(".galerie_choix").show();
 	});
+	
 	$(".galerie_choix a").hover(function(){
 		var nom = $(this).text();
 		$(".categorie").hide();
 		$("#galerie_lien_"+nom).show();
 	});
+	
 	$("footer").mouseleave(function(){
 		$(".galerie_choix").hide("slow");
 		$(".categorie").hide("slow");
 	});
 
-});
+	$("#page_connexion input").focus(function(){
+		$("#background").stop().fadeTo(500,1);
+	});
+	
+	$("#page_connexion input").blur(function(){
+		$("#background").fadeTo(500,0);
+	});
 
-$(window).load(function() {
-	$('#featured').orbit();
-});
-
-function animateGlow(div){
-    div.css({backgroundPosition:"0 0"})
-       .animate({backgroundPosition:"-3000px 0"},25000,"linear",function(){ animateGlow(div); })
-}
-
-$(function(){
-    $("#background").css("opacity",0);
-    $("#page_connexion input").focus(function(){
-        $("#background").stop().fadeTo(500,1);
-    });
-    $(" #page_connexion input").blur(function(){
-        $("#background").fadeTo(500,0);
-    });
-    //animateGlow($("#background"));
+	$("#icones_projets a").mouseenter(function(){
+		var image = $(this).find("img").attr("src");
+		$("#image_projet img").fadeTo(500,0);
+		$("#image_projet img").attr("src",image);
+		$("#image_projet img").fadeTo(500,1);
+	});
 });
